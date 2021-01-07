02e27e91e708685631be8f81bc8dd487da81b8bd
Luis <lgd26308@gmail.com>
2021-01-06 22:09:52 -0700

    worked on components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,9 @@
 import React from "react";
+
+function Header() {
+    return (
+
+    );
+}
+
+export default Header
---
+++ src/components/Wrapper.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+function Wrapper() {
+    return (
+
+    );
+}
+
+export default Wrapper
