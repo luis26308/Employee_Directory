0d492df4fe4278e65b28b9dad957781e326d5c9c
Luis <lgd26308@gmail.com>
2021-01-06 20:56:18 -0700

    added component files and installed bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import Main from './components/Main';
+import Header from './components/Header';
+import Wrapper from './components/Wrapper';
+import NavBar from './components/Navbar';
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-    </div>
-  );
-}
+      <NavBar />
+      <Wrapper>
+        <Header />
+        <Main />
+      </Wrapper>
+
 
 export default App;
